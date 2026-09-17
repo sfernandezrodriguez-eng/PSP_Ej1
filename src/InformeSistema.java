7dc4b269c920b816e18b044891c8d293ff72a7e4
Sergio <sfernandezrodriguez@danielcastelao.org>
2026-09-17 14:26:34 +0200

    Fixing memoriaTotalReservada and adding memoriaTotalReservada2

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -1,18 +1,49 @@
 public class InformeSistema {
 
 
-    public Integer numProcesadores(){
+    public String numProcesadores(){
         int nucleos = Runtime.getRuntime().availableProcessors();
-        return nucleos;
+        return ("Disponibles JVM: " +  nucleos);
     }
 
-    public String MemoriaTotalReservada(){
+    public String memoriaTotalReservada() {
         Runtime runtime = Runtime.getRuntime();
 
-        long memoriaTotal = runtime.totalMemory();
-        long memoriaLibre = runtime.freeMemory();
+
+        long memoriaTotal = runtime.totalMemory() / (1024 * 1024);
+        long memoriaLibre = runtime.freeMemory() / (1024 * 1024);
+        long memoriaMax = runtime.maxMemory() / (1024 * 1024);
         long memoriaUsada = memoriaTotal - memoriaLibre;
-        String respuesta = ("Memoria usada: " + (memoriaUsada / (1024 * 1024)) + " MB");
+        long mPorcentaje = (memoriaUsada * 100) / memoriaTotal;
+
+
+        String respuesta = String.format("%-17s %d MiB\n", "Total reservada:", memoriaTotal) +
+                String.format("%-17s %d MiB\n", "Libre:", memoriaLibre) +
+                String.format("%-17s %d MiB (%d %% de la total)\n", "En uso:", memoriaUsada, mPorcentaje) +
+                String.format("%-17s %d MiB\n", "Máxima (-Xmx):", memoriaMax);
+
         return respuesta;
     }
+
+    public String memoriaTotalReservada2() {
+        Runtime runtime = Runtime.getRuntime();
+        long[] reservado = new long[8 * 1024 * 1024]; // 8 M · 8 bytes = 64 MiB
+
+
+        long memoriaTotal = runtime.totalMemory() / (1024 * 1024);
+        long memoriaLibre = runtime.freeMemory() / (1024 * 1024);
+        long memoriaMax = runtime.maxMemory() / (1024 * 1024);
+        long memoriaUsada = memoriaTotal - memoriaLibre;
+        long mPorcentaje = (memoriaUsada * 100) / memoriaTotal;
+
+
+        String respuesta = String.format("%-17s %d MiB\n", "Total reservada:", memoriaTotal) +
+                String.format("%-17s %d MiB\n", "Libre:", memoriaLibre) +
+                String.format("%-17s %d MiB (%d %% de la total)\n", "En uso:", memoriaUsada, mPorcentaje) +
+                String.format("%-17s %d MiB\n", "Máxima (-Xmx):", memoriaMax);
+
+        return respuesta;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ void main() {
     InformeSistema app = new InformeSistema();
 
 
-    Integer resultado1 = app.numProcesadores();
-    System.out.println("Resultado Prueba 1: " + resultado1);
+    String resultado1 = app.numProcesadores();
+    System.out.println("PROCESADORES\n" +
+            "================================================== \n" + resultado1);
 
-    String resultado2 = app.MemoriaTotalReservada();
-    System.out.println("Resultado Prueba 2: " + resultado2);
+    String resultado2 = app.memoriaTotalReservada();
+    System.out.println("MEMORIA · ANTES\n" +
+            "================================================== \n" + resultado2);
+
+    String resultado3 = app.memoriaTotalReservada2();
+    System.out.println("MEMORIA · DESPUES\n" +
+            "================================================== \n" + resultado3);
 }
